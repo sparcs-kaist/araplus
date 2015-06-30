9b2dfeb9d4eae2577bb5e61dfcd30728c8cce5f9
김동화 <story@StoryKim-MacBook-Pro.local>
2015-06-30 10:41:26 +0900

    add apps in settings

--- araplus/settings.py
+++ araplus/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'board',
+    'grill',
+    'session'
 )
 
 MIDDLEWARE_CLASSES = (
