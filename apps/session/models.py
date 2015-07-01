dc219068268dd406452d3ad8e901d65aea90ed4e
Chang Won Lee <chiyah92@gmail.com>
2015-07-01 21:45:56 +0900

    created model UserProfile and Message

--- apps/session/models.py
+++ apps/session/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "User %s's profile object" % self.user
+
+
+class Message(models.Model):
+    content = models.TextField()
+    sender = models.ForeignKey('UserProfile', related_name='message_sent')
+    receiver = models.ForeignKey('UserProfile', related_name='message_received')
+    created_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Message from %s to %s at %s" % \
+            (sender, receiver, created_time)
