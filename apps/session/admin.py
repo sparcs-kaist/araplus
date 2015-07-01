eabaef94d6edf6782fcd499caf6221624e747f53
Young Seok 'Tony' Kim <93mighty@gmail.com>
2015-07-02 03:24:38 +0900

    applied UserProfile model to register

--- apps/session/admin.py
+++ apps/session/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.session.models import UserProfile
 
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+	list_display = ('user', 'points')
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- apps/session/views.py
+++ apps/session/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from apps.session.models import UserProfile
 
 # Create your views here.
 def user_login(request):
@@ -33,7 +34,9 @@ def user_register(request):
 		first_name = request.POST['first_name']
 		last_name = request.POST['last_name']
 		new_user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name) 
-		new_user.save()
+		new_user_profile = UserProfile(user=new_user)	
+		new_user_profile.save()
+
 		return render(request, 'session/register_complete.html')
 	return render(request, 'session/register.html')
 
