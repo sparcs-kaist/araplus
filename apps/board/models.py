d5fc2c020b8f26eb1a79820dfa893d57203f95ba
kanon <kanon@kaist.ac.kr>
2015-07-02 18:45:03 +0900

    tab size 수정

--- apps/board/models.py
+++ apps/board/models.py
@@ -3,33 +3,33 @@ import apps.session.models
 
 
 class BoardContent(models.Model):
-	content = models.TextField(null=False)
-	created_time = models.DateTimeField(null=False)
-	is_deleted = models.BooleanField(default=False, null=False)
-	is_anonymous = models.BooleanField(default=False, null=False)
+    content = models.TextField(null=False)
+    created_time = models.DateTimeField(null=False)
+    is_deleted = models.BooleanField(default=False, null=False)
+    is_anonymous = models.BooleanField(default=False, null=False)
 
 
 class Attachment(models.Model):
-	file = models.FileField(null=False)
-	board_content = models.ForeignKey('BoardContent', related_name="attachment")
+    file = models.FileField(null=False)
+    board_content = models.ForeignKey('BoardContent', related_name="attachment")
 
 
 class BoardComment(models.Model):
-	post = models.OneToOneField('BoardPost', related_name="comment")
-	# author = models.ForeignKey(session.UserProfile, related_name="board_comment")
+    post = models.OneToOneField('BoardPost', related_name="comment")
+    # author = models.ForeignKey(session.UserProfile, related_name="board_comment")
 
 
 class BoardContentVote(models.Model):
-	board_content = models.ForeignKey('BoardContent', related_name="content_vote")
-	# userprofile = models.ForeignKey(session.UserProfile, related_name="board_comment_vote")
-	is_up = models.BooleanField(null=False)
+    board_content = models.ForeignKey('BoardContent', related_name="content_vote")
+    # userprofile = models.ForeignKey(session.UserProfile, related_name="board_comment_vote")
+    is_up = models.BooleanField(null=False)
 
 
 class BoardReport(models.Model):
-	reason = models.TextField(null=False)
-	created_time = models.DateTimeField(null=False)
-	board_content = models.ForeignKey('BoardContent', related_name="report")
-	# userprofile = models.ForeignKey(session.UserProfile, related_name="board_report")
+    reason = models.TextField(null=False)
+    created_time = models.DateTimeField(null=False)
+    board_content = models.ForeignKey('BoardContent', related_name="report")
+    # userprofile = models.ForeignKey(session.UserProfile, related_name="board_report")
     content = models.TextField(null=False)
     created_time = models.DateTimeField(null=False)
     is_deleted = models.BooleanField(default=False, null=False)
