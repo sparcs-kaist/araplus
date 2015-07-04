c1de0f49e0ff66787f1670ea7a1f4b6562e94d73
ktk1012 <kimtkyeome@kaist.ac.kr>
2015-07-04 22:37:01 +0900

    권한 없는 사용자가 댓글 수정 시도 할 시에 되돌리도록 함

--- apps/board/views.py
+++ apps/board/views.py
@@ -136,19 +136,19 @@ def comment_write(request, pid, error=''):
 
 @login_required(login_url='/session/login')
 def comment_modify(request, error=''):
-    _User = request.user
-    cid = request.POST.get('cid','')
-    _BoardComment = BoardComment.objects.filter(id = cid)
-    if _BoardComment:
-        _BoardComment = _BoardComment[0]
-        if _BoardComment.author_id != _User.id:
-            error = "Not allowd"
-    else:
-        error = "No Comment"
-    if error:
-        return redirect('../')
-    _BoardContent = _BoardComment.board_content
-    if request.method == 'POST':
+    if request.method == "POST":
+        _User = request.user
+        cid = request.POST.get('cid','')
+        _BoardComment = BoardComment.objects.filter(id = cid)
+        if _BoardComment:
+           _BoardComment = _BoardComment[0]
+           if _BoardComment.author_id != _User.id:
+               error = "Not allowd"
+        else:
+            error = "No Comment"
+        if error:
+            return redirect('../')
+        _BoardContent = _BoardComment.board_content
         _UserProfie = _User.userprofile
         content = request.POST.get('content', '')
         if content == '':
@@ -158,4 +158,5 @@ def comment_modify(request, error=''):
         _BoardContent.content = content
         _BoardContent.save()
         return redirect('../')
+    error = "Invalid access"
     return redirect('../')
