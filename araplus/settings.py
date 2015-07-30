16945baf9e78c7681d54bffc777daca2d7850dc3
Shavakan <chiyah92@gmail.com>
2015-07-30 19:04:47 +0900

    changed database to mysql

--- araplus/settings.py
+++ araplus/settings.py
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'araplus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'mysql.cnf'),
+        },
     }
 }
 
